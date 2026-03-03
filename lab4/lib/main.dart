74de38997f16d2393539d3b44f9e40e5fab314d8
Khizar Hameed <khizarhameed2604@gmail.com>
2026-03-03 12:06:34 +0500

    Task1

---
+++ lab4/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text(
+              'Task 1: Safe Scaffold',
+              style: TextStyle(color: Colors.amber),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
